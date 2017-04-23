773a7817257411f1c08b9dfb07aca47e392c04ba
evgenijaZ <evg.zubrich@gmail.com>
2017-04-24 00:27:46 +0300

    added the Calculate method

--- lab1/src/com/company/Function.java
+++ lab1/src/com/company/Function.java
@@ -4,5 +4,15 @@ package com.company;
  * Created by Evgeniia Zubrich on 23.04.2017.
  */
 public class Function {
-    
+    public double  Calculate(int m, int n){
+        short i,j;
+        double s = 0;
+        for (i = 0; i < n; ++i){
+            for (j = 0; j < m; ++j) {
+                if ((j + 1) != 0)
+                    s += (i + j) / (j + 1);
+            }
+        }
+        return s;
+    }
 }
