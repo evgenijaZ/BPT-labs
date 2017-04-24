d3c352ecc61479c7a5a3c884b7769af260b81a8e
evgenijaZ <evg.zubrich@gmail.com>
2017-04-25 00:18:13 +0300

    added input/output in main

--- lab1/src/com/company/Main.java
+++ lab1/src/com/company/Main.java
@@ -1,8 +1,16 @@
 package com.company;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+        Function f = new Function();
+        Scanner in = new Scanner(System.in);
+        System.out.println("Input m:");
+        int m = in.nextInt();
+        System.out.println("Input n:");
+        int n = in.nextInt();
+        System.out.println("Result: " + f.Calculate(m,n));
 
     }
 }
