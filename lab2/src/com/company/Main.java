945331236b082ee943d49d4a91d8928a0659a1af
evgenijaZ <evg.zubrich@gmail.com>
2017-04-26 21:46:15 +0300

    added filling matrix C (A xor B) and calculating sums

--- lab2/src/com/company/Main.java
+++ lab2/src/com/company/Main.java
@@ -4,5 +4,7 @@ public class Main {
 
     public static void main(String[] args) {
         MatrixSolver s = new MatrixSolver(6);
+        s.printMatrices();
+        s.CalculateSums();
     }
 }
--- lab2/src/com/company/MatrixSolver.java
+++ lab2/src/com/company/MatrixSolver.java
@@ -7,31 +7,84 @@ import java.util.Random;
  */
 public class MatrixSolver {
     private int n = 5;
-    private long[][] A = new long[n][n];
+    private long[][] A = null;
+    private long[][] B = null;
+    private long[][] C = null;
+    private long sumMax;
+    private long sumMin;
+
+
     MatrixSolver(int n) {
-        setN(n);
+        if (n < 0) {
+            System.out.println("n must be a positive integer");
+        } else {
+            this.n = n;
+            A = new long[n][n];
+            B = new long[n][n];
+            C = new long[n][n];
+            fillMatrix(A);
+            fillMatrix(B);
+            fillXor(A, B);
+        }
+    }
+
+    void printMatrices() {
+        System.out.println("Matrix A:");
+        printMatrix(A);
+        System.out.println("\nMatrix B:");
+        printMatrix(B);
+        System.out.println("\nMatrix C (A xor B):");
+        printMatrix(C);
+    }
+
+    private void fillXor(long[][] matrixA, long[][] matrixB) {
+        if (C.length == matrixA.length && C.length == matrixB.length) {
+            for (int i = 0; i < C.length; i++) {
+                for (int j = 0; j < C[i].length; j++) {
+                    C[i][j] = matrixA[i][j] ^ matrixB[i][j];
+                }
+            }
+        }
+
     }
 
     public int getN() {
         return n;
     }
 
-    public void setN(int num) {
-        n = num;
-    }
-
     private void fillMatrix(long[][] matrix) {
         final Random random = new Random();
         for (int i = 0; i < matrix.length; i++)
             for (int j = 0; j < matrix[i].length; j++)
-                matrix[i][j] = (long) random.nextInt(50);
+                matrix[i][j] = (long) random.nextInt(10);
+
     }
 
-    public void printMatrix(long[][] matrix) {
-        for (int i = 0; i < matrix.length; i++) {
-            for (int j = 0; j < matrix[i].length; j++)
-                System.out.print(matrix[i][j] + "\t");
+    private void printMatrix(long[][] matrix) {
+        for (long[] a : matrix) {
+            for (long b : a)
+                System.out.print(b + "\t");
             System.out.println();
         }
+    }
+
+    protected void CalculateSums() {
+        System.out.println("Calculating...");
+        sumMax = 0;
+        sumMin = 0;
+        long min;
+        long max;
+        for (int i = 0; i < C.length; i++) {
+            min = C[0][i];
+            max = C[0][i];
+            for (int j = 1; j < C[i].length; j++) {
+                if (C[j][i] > max) max = C[j][i];
+                if (C[j][i] < min) min = C[j][i];
+            }
+            if (i % 2 == 0) sumMax += max;
+            else sumMin += min;
+        }
+        System.out.println("The sum of the largest items: " + sumMax);
+        System.out.println("The sum of the least items: " + sumMin);
     }
 }
