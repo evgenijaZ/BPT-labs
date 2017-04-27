1593cec63da6dec6412b8ed6b4dbe377de270e1f
evgenijaZ <evg.zubrich@gmail.com>
2017-04-28 00:30:42 +0300

    added class StringTransformer

---
+++ lab3/src/com/company/StringTransformer.java
@@ -0,0 +1,13 @@
+package com.company;
+
+/**
+ * Created by Evgeniia Zubrich on 28.04.2017.
+ */
+public class StringTransformer {
+    private StringBuffer myString = null;
+
+    StringTransformer(String string) {
+        myString = new StringBuffer(string);
+    }
+
+}
