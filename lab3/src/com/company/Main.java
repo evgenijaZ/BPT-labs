f0dcaf66fae22e7571368d870eeb8f4822d4c26e
evgenijaZ <evg.zubrich@gmail.com>
2017-05-13 01:56:30 +0300

    added line input and output of the result

--- lab3/src/com/company/Main.java
+++ lab3/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter line:");
+        String str = sc.nextLine();
+        StringTransformer st = new StringTransformer(str);
+        System.out.println(st.ReplaceFirstAndLast());
     }
 }
