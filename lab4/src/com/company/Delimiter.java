3a230ac26c4434d749bd4d92e64c4fd88ca1487f
evgenijaZ <evg.zubrich@gmail.com>
2017-05-14 14:29:42 +0300

    added classes base Letter and Delimiter

---
+++ lab4/src/com/company/Delimiter.java
@@ -0,0 +1,10 @@
+package com.company;
+
+
+public class Delimiter {
+    private char value;
+
+    public Delimiter(char character) {
+        this.value = character;
+    }
+}
---
+++ lab4/src/com/company/Letter.java
@@ -0,0 +1,14 @@
+package com.company;
+
+
+class Letter {
+    private char value;
+
+    Letter(char character) {
+        this.value = character;
+    }
+
+    char getValue() {
+        return value;
+    }
+}
