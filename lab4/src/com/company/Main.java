b63b223f7f30945e9782aedfd20b9c95d97c88fe
evgenijaZ <evg.zubrich@gmail.com>
2017-05-14 15:46:32 +0300

    added method for replace first and last words in sentence

--- lab4/src/com/company/Main.java
+++ lab4/src/com/company/Main.java
@@ -1,8 +1,14 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Scanner sc = new Scanner(System.in);
+        StringBuffer sb = new StringBuffer(sc.nextLine());
+        Sentence s = new Sentence(sb);
+        s.replaceFirstAndLastWords();
+        s.printSentence();
     }
 }
