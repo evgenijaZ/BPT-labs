2ad6c6747f97ff889646e387bb07891060c072c4
evgenijaZ <evg.zubrich@gmail.com>
2017-05-15 20:40:38 +0300

    added class Song

---
+++ lab5/src/com/company/Song.java
@@ -0,0 +1,46 @@
+package com.company;
+
+class Song {
+    private int length;
+    private String name;
+    private String genre;
+
+    Song(String name, String genre, int length) {
+        this.name = name;
+        this.genre = genre;
+        this.length = length;
+    }
+
+    int getLength() {
+        return this.length;
+    }
+
+    void setLength(int len) {
+        this.length = len;
+    }
+
+    String getGenre() {
+        return this.genre;
+    }
+
+    void setGenre(String gen) {
+        this.genre = gen;
+    }
+
+    private String getName() {
+        return this.name;
+    }
+
+    void setName(String nam) {
+        this.genre = nam;
+    }
+
+    int compareTo(Song t) {
+        return this.getGenre().compareTo(t.getGenre());
+    }
+
+    void print() {
+        System.out.println('"' + this.getName() + '"' + "    " + this.getGenre() + " " + this.getLength());
+    }
+}
+
