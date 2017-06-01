27e5ac745721c0a04f39ae368c39a6a34678fce4
evgenijaZ <evg.zubrich@gmail.com>
2017-06-01 20:09:11 +0300

    test classes

--- lab6/src/com/company/Main.java
+++ lab6/src/com/company/Main.java
@@ -3,6 +3,18 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        //test
+        MusicalNode node1 = new MusicalNode <Song>(new Song("Autopilot", "Kodaline", "pop", 259));
+        MusicalNode node2 = new MusicalNode <Song>(new Song("Luck", "American Authors", "rock", 245));
+        MusicalNode node3 = new MusicalNode <Song>(new Song("The nights", "Avicii", "indie", 245));
+        MusicalNode node4 = new MusicalNode <Song>(new Song("It's about time", "Barcelona", "pop", 232));
+        MusicalNode node5 = new MusicalNode <Song>(new Song("Be Clear", "Fiction", "pop", 234));
+        Album <Song> album = new Album <>();
+        album.add(node5);
+        album.add(node4);
+        album.add(node3);
+        album.add(node2);
+        album.add(node1);
     }
+
 }
