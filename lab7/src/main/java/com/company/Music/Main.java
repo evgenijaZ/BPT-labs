b6b81a29d8ed5bece54751153d77fab74f3d9c24
evgenijaZ <evg.zubrich@gmail.com>
2017-09-27 23:39:14 +0300

    tested methods of Stream

--- lab7/src/main/java/com/company/Music/Main.java
+++ lab7/src/main/java/com/company/Music/Main.java
@@ -1,7 +1,7 @@
 package main.java.com.company.Music;
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         //test
         MusicalNode node1 = new MusicalNode <Song>(new Song("Autopilot", "Kodaline", "pop", 259));
         MusicalNode node2 = new MusicalNode <Song>(new Song("Luck", "American Authors", "rock", 245));
@@ -15,6 +15,18 @@ public class Main {
         album.add(node2);
         album.add(node1);
         album.print();
-    }
 
+//        Stream str = new Stream();
+//        str.SerializeObject(album, "B.dat");
+//        System.out.println(((Album)str.DeserializeObject("B.dat")).equals(album));
+//        str.SerializeObject(node1, "A.dat");
+//        System.out.println(((MusicalNode)str.DeserializeObject("A.dat")).toString());
+//        str.SerializeObject(album, "B.dat");
+//        ((Album)str.DeserializeObject("B.dat")).print();
+//        str.SerializeCollection(album, "C.dat");
+//        str.DeserializeCollection("C.dat").print();
+//        str.WriteCollection(album, "D.txt");
+//        (str.ReadCollection("D.txt")).print();
+
+    }
 }
